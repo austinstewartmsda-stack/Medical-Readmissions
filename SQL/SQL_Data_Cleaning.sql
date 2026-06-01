f56e2a1e50ab663b442c96ae0079620a08fa1667
Austin Stewart <austin.stewart.msda@gmail.com>
2026-06-01 09:02:57 -0600

    Add files via upload
    I added the sql file that I'm using to keep track of my sql cleaning steps as I find where I stored them previously.

---
+++ SQL/SQL_Data_Cleaning.sql
@@ -0,0 +1,8 @@
+UPDATE readmit_no_duplicates
+SET
+    Service_Month = TRIM(Service_Month),
+    Service_Month_Date = TRIM(Service_Month_Date),
+    Import_Note = TRIM(Import_Note),
+    Review_Status = TRIM(Review_Status),
+    Batch_ID = TRIM(Batch_ID);
+
